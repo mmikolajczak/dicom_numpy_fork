61e5cc4ef07d979c5ba269efacb74d723e431455
mmikolajczak <mkm0796@gmail.com>
2019-05-10 00:36:54 +0200

    First commit – fork with all changes added

---
+++ setup.py
@@ -0,0 +1,11 @@
+# as minimal as possible
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'dicom_numpy',
+    version = '10.0.0',
+    url = 'https://github.com/mmikolajczak/dicom_numpy_fork',
+    description = 'Fork of dicom_numpy package, with a couple of modifications/fixes',
+    packages = find_packages()
+)
